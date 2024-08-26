ec22ef7f308353ef7e2213b8265bbbf71e984003
CodingWithMK <032180115@ogr.uludag.edu.tr>
2024-08-26 04:15:26 +0200

    Gig adding functionality added.

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+
+// GET ALL GIGS
 router.get('/', (req, res) =>
     Gig.findAll()
         .then(gigs => {
@@ -11,5 +13,30 @@ router.get('/', (req, res) =>
         })
         // .then(gigs => res.render('gigs', {gigs}))
         .catch(err => console.log(err)));
+
+// Add a gig
+router.get('/add', (req, res) => {
+    
+    const data = {
+        title: 'Simple Wordpress website',
+        technologies: 'Wordpress, PHP, HTML, CSS',
+        budget: '$1000',
+        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elit, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        contact_email: 'user2@gmail.com'
+    }
+
+    let {title, technologies, budget, description, contact_email} = data;
+
+    // Insert into table
+    Gig.create({
+        title,
+        technologies,
+        budget,
+        description,
+        contact_email
+    })
+        .then(gig => res.redirect('/gigs'))
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
